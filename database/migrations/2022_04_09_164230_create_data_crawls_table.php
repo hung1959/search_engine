f06f12916482a87db7bf9de85620eac5a81a0fb6
PhatScofield <scofield.nttp@gmail.com>
2022-04-10 01:05:18 +0700

    Migrations for db and some method for search

---
+++ database/migrations/2022_04_09_164230_create_data_crawls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataCrawlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_crawls', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('url');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_crawls');
+    }
+}
