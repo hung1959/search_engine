75ff3f5a5739942fcc2a95f92fbf57be56e93768
loclx-2654 <le.xuan.loc@sun-asterisk.com>
2022-04-09 15:20:58 +0700

    converting data to crawl

--- app/Http/Controllers/CrawlingController.php
+++ app/Http/Controllers/CrawlingController.php
@@ -10,8 +10,37 @@ class CrawlingController extends Controller
     public function index()
     {
         $dom = new Dom;
-        $dom->loadStr('<div class="all"><p>Hey bro, <a href="google.com">click here</a><br /> :)</p></div>');
-        $a = $dom->find('a')[0];
-        echo $a->text; // "click here"
+        $searchUrl = $this->handleSearchUrl();
+        $dom->loadFromUrl($searchUrl);
+        $result = $dom->outerHtml;
+        $this->regexResult($result);
+    }
+
+    public function handleSearchUrl()
+    {
+        $searchUrl = "https://www.google.com/search?q=";
+        $key = "du lich da nang"; # get from user request
+        $searchKey = $this->convertingSearchKey($key);
+        return "${searchUrl}${searchKey}";
+    }
+
+    public function convertingSearchKey($searchKey)
+    {
+        return str_replace(' ','+', $searchKey);
+    }
+
+    public function regexResult($result)
+    {
+        $firstString = '</div></div><div><div class'; 
+        $splitPos = strpos($result,$firstString);
+        $result = substr($result, $splitPos);
+        $lastString = 'T?m ki&#7871;m c? li?n quan';
+        $splitPos = strpos($result,$lastString);
+        $remove = substr($result, $splitPos);
+        $result = str_replace($remove,"",$result);
+        #$a = preg_match( '/<h3>(.*?)<\/div>/', $result, $match );
+        #$pieces = explode("<h3>", $result);
+    
+        dd($result);
     }
 }
